3a75844e6d0d18cd116120c4094e256ccbe908ed
Ashley <admin@ashleybailey.me>
2023-02-27 02:43:01 +0000

    Initial commit

---
+++ android/src/main/java/com/realashleybailey/plugin/printer/Printer.java
@@ -0,0 +1,11 @@
+package com.realashleybailey.plugin.printer;
+
+import android.util.Log;
+
+public class Printer {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
